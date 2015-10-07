a629cce8b5426189b413e587581c9bbe69d4e339
Floris <sk8_floris@hotmail.com>
2015-10-07 15:22:49 +0200

    small script added to add some papers

---
+++ paper/addpapers.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./paperclient localhost -a 'First author' 'First title' paper1.pdf
+./paperclient localhost -a 'Second author' 'Second title' paper2.pdf
+./paperclient localhost -a 'Third author' 'Third title' paper3.pdf
+./paperclient localhost -a 'Fourth author' 'Fourth title' paper4.pdf
+./paperclient localhost -a 'Fifth author' 'Fifth title' paper5.pdf
+./paperclient localhost -a 'Sixth author' 'Sixth title' paper6.pdf
+./paperclient localhost -l
